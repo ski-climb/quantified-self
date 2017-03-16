896a13cea4ab605a779eedc86dacdc67acdca38e
Molly Brown <mollbrown@gmail.com>
2017-03-16 17:34:43 -0600

    add tests for meal food item populate and delete functionality

--- test/foods-test.js
+++ test/foods-test.js
@@ -1,14 +1,22 @@
-describe('#create-form', function() {
+describe('Manage Foods', function() {
+  var $;
+
+  before(function(){
+    $ = document.getElementById("foods-frame").contentWindow.$;
+  })
 
   beforeEach(function() {
-    //Clear out all the things
     $('#food-list tbody').html('');
     $('#create-form input').val('');
     $('.validation-error').html('');
+    window.localStorage.clear();
+  });
+
+  afterEach(function() {
+    window.localStorage.clear();
   });
 
   context('validations', function() {
-
     it('will tell me if I fail to enter a name', function() {
       $('#calories-field input').val('35');
       $('#add-food').click();
@@ -34,6 +42,52 @@ describe('#create-form', function() {
       var caloriesValidationContent = $("#calories-field .validation-error").text();
       assert.equal(caloriesValidationContent, "");
     });
-
   });
+
+  context('functionality', function() {
+    it('can add a food to the foods table', function() {
+      $('#name-field input').val('Banana');
+      $('#calories-field input').val('35');
+      $('#add-food').click();
+
+      var nameContent = $(".food-name").text();
+      var calorieContent = $(".food-calories").text();
+      assert.equal(nameContent, "Banana");
+      assert.equal(calorieContent, "35");
+    });
+
+    it('can delete a food from the foods table', function() {
+      $('#name-field input').val('Banana');
+      $('#calories-field input').val('35');
+      $('#add-food').click();
+      $('.food-delete').click();
+
+      var nameContent = $(".food-name").text();
+      var calorieContent = $(".food-calories").text();
+
+      var deleted = $(".food-name").parents("tr").hasClass("hidden")
+      assert.isTrue(deleted)
+    });
+
+    it('can filter foods by food name', function() {
+      $('#name-field input').val('Banana');
+      $('#calories-field input').val('35');
+      $('#add-food').click();
+
+      $('#name-field input').val('Apple');
+      $('#calories-field input').val('50');
+      $('#add-food').click();
+
+      // Try triggering keypress along with value
+      $('#food-filter input').val('app');
+
+      var nameContent = $(".food-name").text();
+      var calorieContent = $(".food-calories").text();
+
+      assert.equal(nameContent, "Apple");
+      assert.equal(calorieContent, "50");
+    });
+  });
+
+
 });
